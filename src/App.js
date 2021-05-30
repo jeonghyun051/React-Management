1ac425ff0f0a0f5091f75272e54f82de4148795a
kimjeonghyun <rlawjdgus051@naver.com>
2021-05-30 22:32:56 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import './App.css';
+
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const styles = {
+  root: {
+    width: '100%',
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080,
+  }
+};
 
 const customers = [
   {
@@ -22,11 +40,27 @@ const customers = [
 
 const App = () => {
   return (
-    <div>
-      {customers.map((customer) => (
-        <Customer customer={customer} />
-      ))}
-      </div>
+    <Paper className={styles.root}>
+      <Table className={styles.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+
+        {/* 테이블이 화면에 꽉차게 해줌 */}
+        <TableBody> 
+          {customers.map((customer) => (
+            <Customer customer={customer} />
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 const Customer = (props) => {
     const { id, name, birthday, gender, job, image } = props.customer;
 
     return (
-        <div>
-            <h2>{name}</h2>
-            <p>{birthday}</p>
-            <p>{gender}</p>
-            <p>{job}</p>
-            <div>
-                <img src={image} alt="profile" />
-            </div>
-        </div>
+        <TableRow>
+            <TableCell>{id}</TableCell>
+            <TableCell><img src={image} alt="profile" /></TableCell>
+            <TableCell>{name}</TableCell>
+            <TableCell>{birthday}</TableCell>
+            <TableCell>{gender}</TableCell>
+            <TableCell>{job}</TableCell>
+        </TableRow>
     );
 };
 
